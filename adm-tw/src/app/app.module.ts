ff9623382a0905d29d7a295c3b0c9f80d10b6596
Jean Silveira <jeeancarlossilveira@hotmail.com>
2024-10-21 19:57:57 -0300

    Projeto inicial e instalando primeng e template

---
+++ adm-tw/src/app/app.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { HashLocationStrategy, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+import { AppLayoutModule } from './layout/app.layout.module';
+// import { NotfoundComponent } from './demo/components/notfound/notfound.component';
+
+@NgModule({
+    declarations: [AppComponent],
+    imports: [AppRoutingModule, AppLayoutModule],
+    providers: [
+        { provide: LocationStrategy, useClass: PathLocationStrategy },
+    ],
+    bootstrap: [AppComponent],
+})
+export class AppModule {}
